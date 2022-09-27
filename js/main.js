4bcb3df4f54782de3541e566253f22ec7416699c
Paola Kossa <paolakossa@gmail.com>
2022-09-27 17:41:28 -0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const botao = document.querySelector('.botao');
+
+function calcularHipotenusa () {
+	const a = document.querySelector('.aInput').value;
+	const b = document.querySelector('.bInput').value;
+
+	const resultado = document.querySelector('.resultado');
+
+	const hipotenusa = Math.round(Math.sqrt((a*a) + (b*b))); 
+
+	resultado.innerHTML = hipotenusa;
+}; 
+
+botao.addEventListener('click', calcularHipotenusa()); 
